029d34804afedcdbea0564e181adddfb114ddd1b
Ben Holmes <ben.holmes@hashicorp.com>
2023-03-06 20:13:40 +0000

    fixed sentinel policies

--- modules/tfc_configuration/sentinel.tf
+++ modules/tfc_configuration/sentinel.tf
@@ -1,18 +1,9 @@
-data "tfe_workspace_ids" "webinar" {
-  names        = ["webinar-*"]
-  organization = var.tfc_organisation
-}
-
-locals {
-    filtered_workspace_ids = flatten([for id in data.tfe_workspace_ids.webinar.ids: id[*]])
-}
-
 resource "tfe_policy_set" "azure-policies" {
   name          = "azure-sentinel-policies"
   description   = "A set of policies to apply to Azure resources"
   organization  = var.tfc_organisation
   kind          = "sentinel"
-  workspace_ids = local.filtered_workspace_ids
+  workspace_ids = [ for workspace in local.journey_workspace_ids: workspace.id]
 
   vcs_repo {
     identifier         = var.sentinel_policy_repository
